a3b019727cb24aaf54f814bb60c4326f1c423ae9
kohei.sato <night.god.moon.garsu@gmail.com>
2021-06-12 12:45:06 +0900

    basic grammer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1 v-text="msg"></h1>
+    <p v-html="htmlMsg"></p>
+    <p v-text="htmlMsg"></p>
+    <br/>
+    <div>{{ 'current age is ' + age}} </div>
+    <div>{{ 'in 10 years,  it\'ll be ' + (age + 10)}} </div>
+    <div>{{ favoriteFoods }} </div>
+    <br/>
+    <div class="data">{{ $data }}</div>
+    <ul id="example-1">
+      <li v-for="(item, key) in $data" :key='key'>
+        {{ item }} : {{ key}}
+      </li>
+    </ul>
   </div>
-  <router-view />
 </template>
+
+<script lang="ts">
+const favoriteFoods: string[] = ['ramen', 'sushi', 'spagetti']
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'This is sugar project!!!',
+      htmlMsg: '<h2>html message!</h2>',
+      age: 33,
+      favoriteFoods: favoriteFoods
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
