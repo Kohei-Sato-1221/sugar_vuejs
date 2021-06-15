fc708b0de0a9dc41e1d678befa8d9c3cc04f5436
kohei.sato <night.god.moon.garsu@gmail.com>
2021-06-15 11:29:07 +0900

    operation done

--- src/views/Ope.vue
+++ src/views/Ope.vue
@@ -1,20 +1,67 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <button v-on:click="countUp">Click</button>
+    <p>{{ count }}</p>
+    <p v-if="visible">たくさんカウントアップされました！</p>
+    <br/>
+    <div id="array">
+      <ul>
+        <li v-for="(value, key) in myArray" v-bind:key="key">{{ value }}</li>
+      </ul>
+      <input v-model="prefecture" placeholder="prefacture">
+      <button v-on:click="add">add</button>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-const favoriteFoods: string[] = ['ramen', 'sushi', 'spagetti']
 
-export default {
-  name: 'hello',
-  data () {
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+const line = () => {
+  alert('Good!')
+}
+
+export type DataType = {
+  msg: string
+  count: number
+  visible: false
+  myArray: string[]
+  prefecture: string
+}
+
+
+export default new Vue({
+  data(): DataType{
     return {
-      msg: 'Ope page',
+      msg: 'Operation Page',
+      count:0,
+      visible: false,
+      myArray: ["Saitama", "Kanagawa", "Tokyo"],
+      prefecture: ''
+    }
+  },
+
+  methods: {
+    countUp(): void {
+      const newCount = this.count++
+      if (newCount > 5) {
+        this.visible = true       
+      }
+      if (newCount < 1) {
+        line()
+      }
+    },
+    add(): void {
+      if(this.prefecture){
+        this.myArray.push(this.prefecture)
+      }
     }
   }
-}
+})
+
+
 </script>
 
 
